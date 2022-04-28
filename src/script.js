e0d380564ac70a68468d7b93726f85dda92c25d3
pulpxi <hassanhadi17@hotmail.com>
2022-04-29 00:24:41 +0200

    update

--- src/script.js
+++ src/script.js
@@ -4,92 +4,117 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GUI } from 'dat.gui'
 
 
-
-// Dat GUI
+//#region Main Variables
 const gui = new GUI();
-
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
-
-/**
- * Sizes
- */
+let scene, camera, canvas, renderer, controls;
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
+};
+//#endregion
+
+start();
+function start(){
+    // Canvas
+    canvas = document.querySelector('canvas.webgl');
+    // Scene
+    scene = new THREE.Scene();
+    //Renderer
+    renderer = new THREE.WebGLRenderer({
+        canvas: canvas,
+        antialias: true,
+    });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    // Camera
+    camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+    camera.position.x = 1
+    camera.position.y = 1
+    camera.position.z = 1
+    const cameraFolder = gui.addFolder('Camera Settings');
+    cameraFolder.add(camera.position, 'x');
+    cameraFolder.add(camera.position, 'y');
+    cameraFolder.add(camera.position, 'z');
+    scene.add(camera)
+    // Resize Event
+    window.addEventListener('resize', onWindowResize);
+    // Controls
+    controls = new OrbitControls(camera, canvas)
+    controls.enableDamping = true
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+//#region Event Listeners
+function onWindowResize(){
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    debugCamera.aspect = sizes.width / sizes.height;
+    debugCamera.updateProjectionMatrix();
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+}
+//#endregion
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 1
-scene.add(camera)
 
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
 
-/**
- * Cube
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Cube
 const cube = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    new THREE.MeshStandardMaterial({ color: 0xffffff })
 )
 scene.add(cube)
 
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true,
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// Light
+const light = new THREE.DirectionalLight(
+    new THREE.Color(255, 255, 255),
+    .8,
+);
+light.position.set(10, 10, 10);
+scene.add(light);
+const lightHelper = new THREE.DirectionalLightHelper(light);
+scene.add(lightHelper);
 
-/**
- * Animate
- */
-const clock = new THREE.Clock()
-let lastElapsedTime = 0
+const lightFolder = gui.addFolder('Light Settings');
+lightFolder.add(light, 'intensity');
+lightFolder.add(light.position, 'x');
+lightFolder.add(light.position, 'y');
+lightFolder.add(light.position, 'z');
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - lastElapsedTime
-    lastElapsedTime = elapsedTime
 
-    // Update controls
-    controls.update()
 
-    // Render
-    renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+
+
+
+
+
+
+
+
+
+
+
+
+function update(){
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(update);
 }
 
-tick()
+update();
